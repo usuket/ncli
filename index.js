fcece0e8bdba90e7a1b7ee6149087b58f00bdf89
usuket <sy0202002@gmail.com>
2020-03-06 01:11:04 +0900

    Add read stream

--- index.js
+++ index.js
@@ -3,11 +3,25 @@ const fs = require("fs");
 const path = require("path");
 
 const fullPath = path.dirname(require.main.filename);
-const packegePath = path.join(fullPath,"/package.json");
+const packegePath = path.join(fullPath, "/package.json");
 const packageJson = JSON.parse(fs.readFileSync(packegePath));
 
-function main() {
-  console.info("hello", packageJson.version, packegePath);
-}
+// If argv is fileName -> fs.createReadStream(packegePath)
+// No argv -> process.stdout
 
-main();
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: fs.createReadStream(packegePath),
+  output: process.stdout,
+  terminal: false
+});
+
+let number = 0;
+rl.on('line', function (line) {
+  console.log(number, line);
+  number++;
+});
+
+rl.on("close", function () {
+  console.info("done")
+});
